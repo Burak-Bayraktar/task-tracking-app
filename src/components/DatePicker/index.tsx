cce06614792ef172feb36bfdd1b6a72923e33e40
Burak Bayraktar <byrktrhmx@gmail.com>
2023-05-17 17:38:19 +0300

    ADD: Close calendar after clicking outside of it on Header

--- src/components/DatePicker/index.tsx
+++ src/components/DatePicker/index.tsx
@@ -1,15 +1,34 @@
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import "components/DatePicker/style.css";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const DatePicker = () => {
   const [isCalendarOpen, setIsCalendarOpen] = useState<boolean>(false);
   const [value, onChange] = useState<Date>(new Date());
+  const containerRef = useRef<HTMLDivElement>(null);
 
   const handleChange = (e: Date) => {
     // TODO: Add logic to change date later
     console.log(e, "date changed");
+  };
+
+  useEffect(() => {
+    const handleOutsideClick = (e: MouseEvent) => {
+      if (!containerRef.current?.contains(e.target as Node)) {
+        setIsCalendarOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, [isCalendarOpen]);
+
+  const handleCalendarContainerClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
   };
 
   return (
@@ -17,10 +36,11 @@ const DatePicker = () => {
       <div
         className="date-picker-container"
         onClick={() => setIsCalendarOpen(!isCalendarOpen)}
+        ref={containerRef}
       >
         <div className="selected-date">Today</div>
         {isCalendarOpen && (
-          <div className="calendar" onClick={(e) => e.stopPropagation()}>
+          <div className="calendar" onClick={(e) => handleCalendarContainerClick(e)}>
             <Calendar value={value} onClickDay={(e) => handleChange(e)} />
           </div>
         )}
