cf7909b15ab77b6cc73ed7d7136331e8a390487b
Burak Bayraktar <byrktrhmx@gmail.com>
2023-05-19 02:17:09 +0300

    FIX: Wrap Context Providers with BrowserRouter to be able to access router hooks, such as useLocation, inside Contexts

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import useUser from "hooks/useUser";
-import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "views/Dashboard";
 
 function App() {
   useUser();
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/dashboard" />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Navigate to="/dashboard" />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,19 @@ import { TaskProvider } from "contexts/TaskContext.tsx";
 import { ModalProvider } from "contexts/ModalContext.tsx";
 import "index.css";
 import ModalOpener from "modals/ModalOpener.tsx";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <TaskProvider>
-      <ModalProvider>
-        <div className="app-container">
-          <App />
-          <ModalOpener />
-        </div>
-      </ModalProvider>
-    </TaskProvider>
+    <BrowserRouter>
+      <TaskProvider>
+        <ModalProvider>
+          <div className="app-container">
+            <App />
+            <ModalOpener />
+          </div>
+        </ModalProvider>
+      </TaskProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
