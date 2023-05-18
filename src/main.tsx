e44f68e5422603a196281c416f61d449c9125096
Burak Bayraktar <byrktrhmx@gmail.com>
2023-05-18 22:45:30 +0300

    ADD: Wrap App with TaskProvider and ModalProvider and import ModalOpener component

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { TaskProvider } from "contexts/TaskContext.tsx";
+import { ModalProvider } from "contexts/ModalContext.tsx";
 import "index.css";
+import ModalOpener from "modals/ModalOpener.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <div className="app-container">
-      <App />
-    </div>
+    <TaskProvider>
+      <ModalProvider>
+        <div className="app-container">
+          <App />
+          <ModalOpener />
+        </div>
+      </ModalProvider>
+    </TaskProvider>
   </React.StrictMode>
 );
