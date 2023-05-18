9d66a0215e592bb1ee562c3685ec23fdb467d236
Burak Bayraktar <byrktrhmx@gmail.com>
2023-05-18 22:44:13 +0300

    ADD: Add ModalOpener component to open requested modal(s) from global component

---
+++ src/modals/ModalOpener.tsx
@@ -0,0 +1,28 @@
+import { useModal } from "hooks/useModal";
+import ReportModal from "./ReportModal";
+import "modals/ModalOpener.css";
+
+const possibleModals = [
+  {
+    name: "Report",
+    element: ReportModal,
+  },
+];
+
+const ModalOpener = () => {
+  const { modals } = useModal();
+
+  if (modals?.length === 0) return null;
+
+  return (
+    <div className="modal-opener">
+      {modals?.map((modal) => {
+        const modalToRender = possibleModals.find((m) => m.name === modal.name);
+        if (!modalToRender) return null;
+        return <modalToRender.element />;
+      })}
+    </div>
+  );
+};
+
+export default ModalOpener;
