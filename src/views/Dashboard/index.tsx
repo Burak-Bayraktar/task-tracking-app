f8e2365195d13bc1e805efa2b731a363ef6e0417
Burak Bayraktar <byrktrhmx@gmail.com>
2023-05-19 21:33:39 +0300

    ADD: Navigate URL to 'date' search param automatically on view first rendered

--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -1,10 +1,26 @@
 import Header from "components/Dashboard/Header";
 import TaskTrackingSection from "components/Dashboard/TaskTrackingSection";
+import useDate from "hooks/useDate";
 import useUser from "hooks/useUser";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
 import "views/Dashboard/style.css";
 
 const Dashboard = () => {
   const { activeUser } = useUser();
+  const { date } = useDate();
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    const today = new Date();
+    const day = today.getDate();
+    const month = today.getMonth() + 1;
+    const year = today.getFullYear();
+
+    if (!date) {
+      navigate(`?date=${year}-${month}-${day}`);
+    }
+  }, []);
 
   return (
     <div className="dashboard">
