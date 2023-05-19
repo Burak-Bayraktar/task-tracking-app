89e9f5fb605af14b73357411ef193cfbf5244699
Burak Bayraktar <byrktrhmx@gmail.com>
2023-05-19 20:55:52 +0300

    ADD: Update TaskContext and add CRUD functions to manage tasks across application & Refactor code

--- src/contexts/TaskContext.tsx
+++ src/contexts/TaskContext.tsx
@@ -1,128 +1,119 @@
 import useDate from "hooks/useDate";
 import { createContext, useEffect, useState } from "react";
 import { Task, TaskColumn } from "types";
-import { convertToTasksWithColumn } from "utils";
+import { convertToTasksWithColumn, filterTasksByDate } from "utils";
 
 type TaskContextType = {
   tasks: Task[];
   filteredTasks: Task[];
   tasksWithColumns: TaskColumn[];
-  addTask: (task: Task) => void;
+  manageTasks: (taskModifications: Task[], action: 'add' | 'update' | 'delete') => void;
   filterByDate: (date: string) => void;
   filterBySearchKey: (searchKey: string) => void;
 };
 
-const TaskProps: TaskContextType = {
-  tasks: [],
-  filteredTasks: [],
-  tasksWithColumns: [],
-  addTask(task) {
-    console.log("addTask", task);
-  },
-  filterByDate(date) {
-    console.log("filterByDate", date);
-  },
-  filterBySearchKey(searchKey) {
-    console.log("search key", searchKey);
-  },
-};
-
 const getItemsFromLocalStorage = () => {
   const tasks = localStorage.getItem("tasks");
-  const tasksWithColumns = localStorage.getItem("tasksWithColumns");
-
-  return { tasks, tasksWithColumns };
+  return { tasks };
 };
 
-const TaskContext = createContext<TaskContextType>(TaskProps);
+const initialTaskColumns: TaskColumn[] = [
+  {
+    title: "Backlog",
+    type: "BACKLOG",
+    tasks: [],
+  },
+  {
+    title: "In Progress",
+    type: "IN_PROGRESS",
+    tasks: [],
+  },
+  {
+    title: "Done",
+    type: "DONE",
+    tasks: [],
+  },
+];
+
+const TaskContext = createContext<TaskContextType>({
+  tasks: [],
+  filteredTasks: [],
+  tasksWithColumns: initialTaskColumns,
+  manageTasks() {},
+  filterByDate() {},
+  filterBySearchKey() {},
+});
 
 export const TaskProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [filteredTasks, setFilteredTasks] = useState<Task[]>([]);
-  const [tasksWithColumns, setTasksWithColumns] = useState<TaskColumn[]>([
-    {
-      title: "Backlog",
-      type: "BACKLOG",
-      tasks: [],
-    },
-    {
-      title: "In Progress",
-      type: "IN_PROGRESS",
-      tasks: [],
-    },
-    {
-      title: "Done",
-      type: "DONE",
-      tasks: [],
-    },
-  ]);
+  const [tasksWithColumns, setTasksWithColumns] = useState<TaskColumn[]>(initialTaskColumns);
 
   const { dateAsLocaleDateString } = useDate();
 
   useEffect(() => {
-    const { tasks, tasksWithColumns } = getItemsFromLocalStorage();
-
+    const { tasks } = getItemsFromLocalStorage();
+    
     if (tasks) {
-      setTasks(JSON.parse(tasks) as Task[]);
-      setFilteredTasks(JSON.parse(tasks) as Task[]);
-    }
+      const parsedTasks = JSON.parse(tasks) as Task[];
 
-    if (tasksWithColumns) {
-      setTasksWithColumns(JSON.parse(tasksWithColumns) as TaskColumn[]);
+      const filteredByDate = filterTasksByDate(parsedTasks, dateAsLocaleDateString!);
+      const result = convertToTasksWithColumn(filteredByDate);
+
+      setTasks(parsedTasks);
+      setFilteredTasks(filteredByDate);
+      setTasksWithColumns(result);
     }
-  }, []);
+  }, [dateAsLocaleDateString]);
 
   useEffect(() => {
     filterByDate(dateAsLocaleDateString!);
   }, [dateAsLocaleDateString]);
 
-  const addTask = (task: Task) => {
-    const newTasks = [...tasks, task];
-    const updatedTasksWithColumns = [...tasksWithColumns];
-
-    const column = updatedTasksWithColumns.find(
-      (column) => column.type === task.status.type
-    );
-
-    if (column) {
-      column.tasks.push(task);
-    } else {
-      updatedTasksWithColumns.push({
-        title: task.status.title,
-        type: task.status.type,
-        tasks: [task],
-      });
+  const manageTasks = (taskModifications: Task[], action: 'add' | 'update' | 'delete') => {
+    let newTasks;
+    switch (action) {
+      case 'add':
+        newTasks = [...tasks, ...taskModifications];
+        break;
+      case 'update':
+        newTasks = tasks.map(task => {
+          const updatedTask = taskModifications.find(t => t.id === task.id);
+          return updatedTask ? updatedTask : task;
+        });
+        break;
+      case 'delete':
+        newTasks = tasks.filter(task => !taskModifications.find(t => t.id === task.id));
+        break;
+      default:
+        newTasks = [...tasks];
     }
 
-    setTasksWithColumns(tasksWithColumns);
-    setTasks(newTasks);
+    const updatedTasksWithColumns = convertToTasksWithColumn(newTasks);
 
-    updateLocalStorage(newTasks, updatedTasksWithColumns);
+    setTasks(newTasks);
+    setTasksWithColumns(updatedTasksWithColumns);
+    filterByDate(dateAsLocaleDateString!, newTasks);
+    updateLocalStorage(newTasks);
   };
 
   const updateLocalStorage = (
     newTasks: Task[],
-    tasksWithColumns: TaskColumn[]
   ) => {
     localStorage.setItem("tasks", JSON.stringify(newTasks));
-    localStorage.setItem("tasksWithColumns", JSON.stringify(tasksWithColumns));
   };
 
-  const filterByDate = (date: string) => {
-    if (tasks.length === 0) {
+  const filterByDate = (date: string, newTasks?: Task[]) => {
+    if (!tasks || tasks.length === 0) {
       return;
     }
 
-    // TODO: filter tasks by date
-    const filteredTasks = tasks.filter((task) => {
-      const convertedTaskDate = new Date(task.createdAt).toLocaleDateString(
-        "tr-TR"
-      );
-      return convertedTaskDate === date;
-    });
-
+    const filteredTasks = filterTasksByDate(
+      newTasks?.length ? newTasks : tasks,
+      date
+    );
     const convertedTasks = convertToTasksWithColumn(filteredTasks);
 
     console.info(`Tasks filtered by date: ${date}`, filteredTasks);
@@ -140,16 +131,20 @@ export const TaskProvider: React.FC<{ children: React.ReactNode }> = ({
     console.log("search key", searchKey);
   };
 
-  const values = {
-    tasks,
-    filteredTasks,
-    tasksWithColumns,
-    addTask,
-    filterByDate,
-    filterBySearchKey,
-  };
-
-  return <TaskContext.Provider value={values}>{children}</TaskContext.Provider>;
+  return (
+    <TaskContext.Provider 
+      value={{
+        tasks,
+        filteredTasks,
+        tasksWithColumns,
+        manageTasks,
+        filterByDate,
+        filterBySearchKey,
+      }}
+    >
+      {children}
+    </TaskContext.Provider>
+  );
 };
 
 export default TaskContext;
