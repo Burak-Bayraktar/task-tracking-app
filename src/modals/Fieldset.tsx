edae86fe46c6b7bea2c8640aa0beb7f65d1480df
Burak Bayraktar <byrktrhmx@gmail.com>
2023-05-19 20:58:01 +0300

    ADD: Update ReportModal to add, update and delete tasks from there

---
+++ src/modals/Fieldset.tsx
@@ -0,0 +1,38 @@
+import { Task, TaskItemTitle } from "types";
+
+const statuses: TaskItemTitle[] = ["Bug", "Feature", "Refactor"];
+
+type FieldsetProps = {
+  index: number;
+  reportedTask?: Task;
+};
+
+const Fieldset = ({ index, reportedTask }: FieldsetProps) => (
+  <fieldset name={`task-fieldset-${index + 1}`} key={reportedTask ? reportedTask.id : `empty-${index}`} data-task-id={reportedTask ? reportedTask.id : ''}>
+    <div className="report-row">
+      <legend>Field {index + 1}</legend>
+      <input type="text" hidden name={`task-id-${index + 1}`} value={reportedTask ? reportedTask.id : ''} />
+      <input type="text" hidden name={`task-status-${index + 1}`} value={reportedTask ? `${reportedTask.status.title}-${reportedTask.status.type}` : ''} />
+      <select name={`task-type-${index + 1}`} className="task-type">
+        {statuses.map((status) => (
+          <option
+            value={status.toLowerCase()}
+            selected={
+              reportedTask &&
+              reportedTask.type.title.toLowerCase() === status.toLowerCase()
+            }
+          >
+            {status}
+          </option>
+        ))}
+      </select>
+      <textarea
+        name={`task-desc-${index + 1}`}
+        rows={1}
+        defaultValue={reportedTask ? reportedTask.description : ""}
+      />
+    </div>
+  </fieldset>
+);
+
+export default Fieldset;
--- src/modals/ReportModal.tsx
+++ src/modals/ReportModal.tsx
@@ -1,16 +1,28 @@
-import { useEffect } from "react";
-import { useTask } from "hooks/useTask";
-import { TaskTrackingColumnTitle, TaskItemTitle, TaskItemType } from "types";
-import "modals/ReportModal.css";
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { Task, TaskItemTitle, TaskItemType, TaskTrackingColumnTitle, TaskTrackingColumnType } from "types";
 import { useModal } from "hooks/useModal";
+import { useTask } from "hooks/useTask";
+import useUser from "hooks/useUser";
+import useDate from "hooks/useDate";
 
-const statuses: TaskTrackingColumnTitle[] = ["Backlog", "In Progress", "Done"];
+import { filterTasksByDate, filterTasksByUser } from "utils";
+import Fieldset from "modals/Fieldset";
+import "modals/ReportModal.css";
 
 const ReportModal = () => {
-  const { addTask } = useTask();
+  const { manageTasks, filteredTasks } = useTask();
   const { removeModal } = useModal();
+  const { activeUser } = useUser();
+  const { date, dateAsLocaleDateString } = useDate();
+  const [reportedTasks, setReportedTasks] = useState<Task[]>();
+
+  const DAILY_REPORT_LIMIT = 3;
 
   useEffect(() => {
+    // TODO: don't forget to add cleanup and remove the event listener.
+    // TODO: also, this is not a good practice, don't use querySelectorAll.
+    // TODO: instead, use useRef and pass the ref to the textarea.
     document.querySelectorAll(".report-row textarea")?.forEach((textarea) => {
       textarea.addEventListener("input", function (this: HTMLTextAreaElement) {
         this.style.height = "auto";
@@ -19,23 +31,135 @@ const ReportModal = () => {
     });
   }, []);
 
+  useEffect(() => {
+    if (activeUser) {
+      const filteredTasksByUser = filterTasksByUser(
+        filteredTasks,
+        activeUser.id
+      );
+
+      const filteredTasksByDate = filterTasksByDate(filteredTasksByUser, dateAsLocaleDateString!);
+
+      setReportedTasks(filteredTasksByDate);
+    }
+  }, [activeUser?.id]);
+
+  const closeModal = () => {
+    removeModal("Report");
+  };
+
+  let receivedTasks: { type: 'ADD' | 'REMOVE' | 'UPDATE', task: Task }[] = [];
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    console.log("Submitted");
+    if (!activeUser) {
+      alert("Please select a user to continue reporting.");
+      return;
+    }
+
+    const formData = new FormData(e.currentTarget);
+
+    for (let i = 0; i < DAILY_REPORT_LIMIT; i++) {
+      const taskType = formData.get(`task-type-${i + 1}`) as TaskItemTitle;
+      const taskDesc = formData.get(`task-desc-${i + 1}`) as string;
+      const taskId = formData.get(`task-id-${i + 1}`) as string;
+      const taskStatus = formData.get(`task-status-${i + 1}`) as string;
+      const taskStatusTitle = taskStatus?.split('-')[0] as TaskTrackingColumnTitle;
+      const taskStatusType = taskStatus?.split('-')[1] as TaskTrackingColumnType;
+
+      let shouldUpdate = false;
+
+      if (reportedTasks?.length) {
+        shouldUpdate = reportedTasks[i]?.description !== taskDesc || reportedTasks[i]?.type.title !== taskType;
+      }
+      
+      if (taskId && shouldUpdate) {
+        receivedTasks = [...receivedTasks, { type: 'UPDATE', task: {
+          id: taskId,
+          description: taskDesc,
+          status: {
+            id: 1,
+            title: taskStatusTitle,
+            type: taskStatusType,
+          },
+          type: {
+            id: 1,
+            title: taskType,
+            type: taskType as TaskItemType,
+          },
+          updatedAt: date ? new Date(date) : new Date(),
+          createdAt: date ? new Date(date) : new Date(),
+          author: activeUser,
+        }}];
+      }
+
+      if (!taskId && taskDesc) {
+        receivedTasks = [...receivedTasks, { type: 'ADD', task: {
+          id: uuidv4(),
+          description: taskDesc,
+          status: {
+            id: 1,
+            title: 'Backlog',
+            type: 'BACKLOG',
+          },
+          type: {
+            id: 1,
+            title: taskType,
+            type: taskType as TaskItemType,
+          },
+          updatedAt: date ? new Date(date) : new Date(),
+          createdAt: date ? new Date(date) : new Date(),
+          author: activeUser,
+        }}];
+      }
+
+      if (!taskDesc && taskId) {
+        receivedTasks = [...receivedTasks, { type: 'REMOVE', task: {
+          id: taskId,
+          description: taskDesc,
+          status: {
+            id: 1,
+            title: taskStatusTitle,
+            type: taskStatusType,
+          },
+          type: {
+            id: 1,
+            title: taskType,
+            type: taskType as TaskItemType,
+          },
+          updatedAt: date ? new Date(date) : new Date(),
+          createdAt: date ? new Date(date) : new Date(),
+          author: activeUser,
+        }}];
+      }
+    }
+
+    const tasksAdded = receivedTasks.filter(task => task.type === 'ADD').map(task => task.task);
+    const tasksRemoved = receivedTasks.filter(task => task.type === 'REMOVE').map(task => task.task);
+    const tasksUpdated = receivedTasks.filter(task => task.type === 'UPDATE').map(task => task.task);
+
+    if (tasksAdded.length) {
+      manageTasks(tasksAdded, 'add');
+    }
+    
+    if (tasksUpdated.length) {
+      manageTasks(tasksUpdated, 'update');
+    }
+
+    if (tasksRemoved.length) {
+      manageTasks(tasksRemoved, 'delete');
+    }
+  
+    closeModal();
   };
-
-
-
-  const closeModal = () => {
-    removeModal('Report');
-  }
 
   return (
     <dialog open className="report-modal">
       <header>
         <h1 className="title">Report Your Daily Tasks</h1>
-        <span className="close-icon" onClick={closeModal}>X</span>
+        <span className="close-icon" onClick={closeModal}>
+          X
+        </span>
         <p className="description">
           Please report your daily tasks below. You should enter the task type,
           task name, and estimated time you will spent on the task. You can also
@@ -43,19 +167,15 @@ const ReportModal = () => {
         </p>
       </header>
       <form action="post" onSubmit={handleSubmit}>
-        {[1, 2, 3].map((i) => (
-          <fieldset>
-            <div className="report-row">
-              <legend>Field {i}</legend>
-              <select name={`task-type-${i}`} className="task-type">
-                {statuses.map((status) => (
-                  <option value={status.toLowerCase()}>{status}</option>
-                ))}
-              </select>
-              <textarea name={`task-desc-${i}`} rows={1}></textarea>
-            </div>
-          </fieldset>
-        ))}
+        {new Array(DAILY_REPORT_LIMIT)
+          .fill("")
+          .map((_, index) =>
+            reportedTasks && reportedTasks[index] ? (
+              <Fieldset index={index} reportedTask={reportedTasks[index]} />
+            ) : (
+              <Fieldset index={index} />
+            )
+          )}
         <div className="submit-button-container">
           <button type="submit">Submit</button>
         </div>
