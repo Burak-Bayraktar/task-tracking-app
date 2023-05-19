81c779e202cf992e4b907d2b475aa6fc105906e4
Burak Bayraktar <byrktrhmx@gmail.com>
2023-05-19 20:51:50 +0300

    ADD: Add filterTasksByDate, filterTasksByUser and convertToTaskWithColumn functions to utils.ts

---
+++ src/utils/index.ts
@@ -0,0 +1,51 @@
+import { Task, TaskColumn } from "types";
+
+const convertToTasksWithColumn = (tasks: Task[]) => {
+  const columns: TaskColumn[] = [
+    {
+      title: "Backlog",
+      type: "BACKLOG",
+      tasks: [],
+    },
+    {
+      title: "In Progress",
+      type: "IN_PROGRESS",
+      tasks: [],
+    },
+    {
+      title: "Done",
+      type: "DONE",
+      tasks: [],
+    },
+  ];
+
+  tasks.forEach((task) => {
+    const column = columns.find((column) => column.type === task.status.type);
+    if (column) {
+      column.tasks.push(task);
+    }
+  });
+
+  return columns;
+};
+
+const filterTasksByDate = (tasks: Task[], date: string) => {
+  const filteredTasks = tasks.filter((task) => {
+    const convertedTaskDate = new Date(task.createdAt).toLocaleDateString(
+      "tr-TR"
+    );
+    return convertedTaskDate === date;
+  });
+
+  return filteredTasks;
+};
+
+const filterTasksByUser = (tasks: Task[], userId: string) => {
+  const filteredTasks = tasks.filter((task) => {
+    return task.author.id === userId;
+  });
+
+  return filteredTasks;
+};
+
+export { convertToTasksWithColumn, filterTasksByDate, filterTasksByUser };
