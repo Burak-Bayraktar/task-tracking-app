6d6f6e15849d33881202d161a3a0e7c7c8fa9bb6
Burak Bayraktar <byrktrhmx@gmail.com>
2023-05-19 22:49:57 +0300

    ADD: Add Filter by 'search' key feature

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,20 +1,57 @@
 import "components/SearchBar/style.css";
 import searchIcon from "assets/icons/search.svg";
+import { useTask } from "hooks/useTask";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router";
+import useDate from "hooks/useDate";
 
 const SearchBar = () => {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const [searchKey, setSearchKey] = useState<string>("");
+  const { filterBySearchKey, tasks } = useTask();
+  const navigate = useNavigate();
+  const { search } = useLocation();
+  const { dateAsLocaleDateString } = useDate();
 
-    // TODO: Add logic to search later
-    const test = new FormData(e.target as HTMLFormElement)
-    console.log(test.get("search"))
+  useEffect(() => {
+    updateSearchFromUrl();
+  }, [search]);
+
+  useEffect(() => {
+    setSearchKey("");
+    const searchParams = new URLSearchParams(search);
+    searchParams.delete("search");
+    navigate(`?${searchParams.toString()}`);
+  }, [dateAsLocaleDateString, tasks]);
+
+  const updateSearchFromUrl = () => {
+    const searchParams = new URLSearchParams(search);
+    const searchKey = searchParams.get("search");
+
+    if (searchKey) {
+      setSearchKey(searchKey);
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newSearchKey = e.target.value;
+    setSearchKey(newSearchKey);
+    updateUrl(newSearchKey);
+    filterBySearchKey(newSearchKey);
+  };
+
+  const updateUrl = (newSearchKey: string) => {
+    const searchParams = new URLSearchParams(search);
+    if (newSearchKey !== "") {
+      searchParams.set("search", newSearchKey);
+    } else {
+      searchParams.delete("search");
+    }
+    navigate(`?${searchParams.toString()}`);
   };
 
   return (
     <div className="search-bar-container">
-      <form action="post" onSubmit={handleSubmit}>
-        <input type="text" name="search" placeholder="Search" />
-      </form>
+      <input type="text" name="search" value={searchKey} onChange={handleChange} placeholder="Search" />
       <img src={searchIcon} alt="search" />
     </div>
   );
